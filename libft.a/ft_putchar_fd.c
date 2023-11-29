9cd38a140e2123076ac88d6d65f3fdefae309066
momazouz <122931037+40014@users.noreply.github.com>
2023-11-29 15:09:46 +0100

    Update ft_putchar_fd.c

--- libft.a/ft_putchar_fd.c
+++ libft.a/ft_putchar_fd.c
@@ -11,15 +11,10 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <unistd.h>
+
 void	ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, 1);
 	
 }
-int main()
-{
-	char c = 'd';
-	ft_putchar_fd(c, 2);	
-	write(1, "\n", 1);
-}
+
