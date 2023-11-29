bb481d52c33217995d2d4767764e3afcb3e7b6a2
momazouz <122931037+40014@users.noreply.github.com>
2023-11-29 15:12:56 +0100

    Update ft_strlcat.c

--- libft.a/ft_strlcat.c
+++ libft.a/ft_strlcat.c
@@ -11,7 +11,6 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 size_t	ft_strlcat(char *dest, char *src, size_t dstsize)
 {
